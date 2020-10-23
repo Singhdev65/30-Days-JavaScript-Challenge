935463762ccdf4411bc3b12904ebf2f9d1959b4c
Prince Kumar <70108171+Singhdev65@users.noreply.github.com>
2020-10-24 00:07:50 +0530

    Add files via upload

---
+++ Day-4/q5.js
@@ -0,0 +1,16 @@
+/* Write a JS function that returns true
+if the provided predicate function returns 
+true for all elements in the collection, false
+otherwise. */
+
+const isEveryElem = (arr, fn) => {
+    for(let i=0; i<arr.length; i+=1) {
+        if(!fn(arr[i])) {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(isEveryElem([1,2,3,4,5], (x) => x > 0));
+console.log(isEveryElem([1,2,3,4,5], (x) => x > 3));
